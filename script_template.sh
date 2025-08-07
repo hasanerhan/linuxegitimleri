e07bdc8bb3060c20a2eb78b131142662efc46b59
Hasan Erhan AYDINOĞLU <47900267+hasanerhan@users.noreply.github.com>
2025-08-07 23:08:15 +0300

    Create script_template.sh

---
+++ script_template.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+## AUTHORS     : ISIM SOYISIM
+# EMAILS      : Email
+# START DATE  : 2024/02/26
+# UPDATE DATE : 2025/05/24
+# VERSION     : Check REV variable
+# DESCRIPTION : This script blah blah. 
+# CHANGELOG   : v1.0 First Publication
